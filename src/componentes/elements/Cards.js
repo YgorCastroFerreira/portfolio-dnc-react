3acd69d33fc8b647cca598edff05554e71be16a3
Ygor Castro Ferreira <76598068+YgorCastroFerreira@users.noreply.github.com>
2023-11-16 10:26:17 -0300

    att com javascript avançado

--- src/componentes/elements/Cards.js
+++ src/componentes/elements/Cards.js
@@ -1,20 +1,34 @@
 import styles from './Cards.module.css'
 import ButtonB from './ButtonB'
-
+import {useState} from 'react'
 
 function Cards({img, title, tech, description, repo, site}){
+
+    const [info,setInfo] = useState(false)
+
+    function InfoOn(){
+        setInfo(true)
+    }
+    function InfoOff(){
+        setInfo(false)
+    }
+
     return(
-        <div className={styles.cards}>
-            <a href={site}>
-                <img src={img}></img>
+        <div onMouseLeave={InfoOff} className={styles.cards}>
+            <a onMouseEnter={InfoOn} href={site}>
+                <img src={img} alt='error...'></img>
             </a>
-            
-            <section>
-                <h3>{title}</h3>
-                <p><strong>Tecnologia: </strong>{tech}</p>
-                <p>{description}</p>
-                <ButtonB text='Acesse o repositório' link={repo}/>
-            </section>
+
+            {info === true &&(
+                <section>
+                    <h3>{title}</h3>
+                    <p><strong>Tecnologia: </strong>{tech}</p>
+                    <p>{description}</p>
+                    <ButtonB text='Acesse o repositório' link={repo}/>
+                </section>
+
+            ) }
+
         </div>
     )
 }
--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,13 +1,46 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
 import ButtonB from '../elements/ButtonB'
+import {useEffect, useState} from 'react'
 
 
 function Presentation(){
+    const [text,setText] = useState('');
+    const toRotate = ['Yusuke Uramechi', 'Desenvolvedor Front-end'];
+    const [loop,setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100);
+    
+
+    
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1):fullText.substring(0,text.length+1);
+
+        setText(updatedText);
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === ''){
+                setIsDeleting(false);
+                setDelta(period);
+                setLoop(loop+1);
+        }
+    }
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+    },[text])
+
     return(
         <div className={styles.presentation} id='Presentation'>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou o Yusuke Uramechi</h1>
+            <h1>Olá, eu sou {text} </h1>
             <p>
                 Sou um apaixonado por tecnologia e soluções inovadoras.Como<br></br>
                  Product Manager, eu tenho o compromisso de <br></br>
